00b3644454830c973210746b55f366d49b5f3011
Lehanchic25 <136587212+Lehanchic25@users.noreply.github.com>
2026-03-30 19:25:31 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+//This thing is responsible for all this chaos
+Events.on(EventType.WorldLoadEvent, () => {
+    Vars.state.rules.teams.get(Team.sharded).blockHealthMultiplier = 0.1;
+    Vars.state.rules.teams.get(Team.sharded).buildSpeedMultiplier = 10;
+    Vars.state.rules.buildCostMultiplier = 10;
+    Blocks.coreShard.itemCapacity *= 10;
+    Blocks.coreFoundation.itemCapacity *= 10;
+    Blocks.coreNucleus.itemCapacity *= 10;
+    Blocks.coreBastion.itemCapacity *= 10;
+    Blocks.coreCitadel.itemCapacity *= 10;
+    Blocks.coreAcropolis.itemCapacity *= 10;
+    Blocks.mechanicalDrill.drillTime *= 1.25;
+});
+
+//Some customization to description of this mod
+const mod = Vars.mods.locateMod("hardcore-mod");
+mod.meta.displayName = "[orange]Hardcore Mod";
+mod.meta.description = "[red] A single JS script that makes game a lot harder. \n \n [green]Thx [blue]Sputnic [green]for optimization and the idea of changing statistics using js script \n \n [orange]This mod: \n [white] 1. Makes all blocks cost 10 times more \n 2. Makes hp of blocks 10 times less \n 3. Makes itemCapacity in all Cores 10 times more (so you could place some blocks) \n 4. Makes buildCostMultiplier = 10 (so you can place blocks and not wait for eternity) \n 5. Idk makes early game a lil bit harder (drillTime of mechanicalDrill is *= 1.25)"
+mod.meta.author = "[green]Lehanchic25, [blue] Sputnuc"
